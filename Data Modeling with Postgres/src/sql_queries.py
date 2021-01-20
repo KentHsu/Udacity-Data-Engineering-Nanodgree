4365049047787186b593fb78a7a17102c34bb9c9
luffy1610 <luffy1610@gmail.com>
2021-01-20 21:52:12 +0800

    feat: complete drop/create table queries in sql_queries.py

--- Data Modeling with Postgres/src/sql_queries.py
+++ Data Modeling with Postgres/src/sql_queries.py
@@ -1,27 +1,70 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id SERIAL PRIMARY KEY,
+        start_time TIMESTAMP REFERENCES time (start_time),
+        user_id INT REFERENCES users (user_id),
+        level VARCHAR,
+        song_id VARCHAR REFERENCES songs (song_id),
+        artist_id VARCHAR REFERENCES artists (artist_id),
+        session_id INT,
+        location TEXT,
+        user_agent TEXT
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INT PRIMARY KEY,
+        first_name VARCHAR,
+        last_name VARCHAR,
+        gender CHAR(1),
+        level VARCHAR
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id VARCHAR PRIMARY KEY,
+        title VARCHAR,
+        artist_id VARCHAR REFERENCES artists (artist_id),
+        year INT,
+        duration FLOAT
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id VARCHAR PRIMARY KEY,
+        name VARCHAR,
+        location TEXT,
+        latitude FLOAT,
+        longitude FLOAT
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time TIMESTAMP PRIMARY KEY,
+        hour INT,
+        day INT,
+        week INT,
+        month INT,
+        year INT,
+        weekday VARCHAR
+    )
 """)
+
 
 # INSERT RECORDS
 
