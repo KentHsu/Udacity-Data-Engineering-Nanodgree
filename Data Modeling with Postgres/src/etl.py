117f84d98161b21e2465b5e7873dffb982c6260c
luffy1610 <luffy1610@gmail.com>
2021-01-20 23:24:57 +0800

    fix: Remove songplay_id in insertion statement
    Removed songplay_id in songplay_table_insertion query to meet songplay_id SERIAL property
    Also removed index when inserting songplay record in process_log_file funciton

--- Data Modeling with Postgres/src/etl.py
+++ Data Modeling with Postgres/src/etl.py
@@ -8,16 +8,16 @@ from sql_queries import *
 def process_song_file(cur, filepath):
     # open song file
     df = pd.read_json(filepath, lines=True)
-
+    
     # insert artist record
     artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
-
+    
     # insert song record
     song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
 
-
+    
 def process_log_file(cur, filepath):
     # open log file
     df = pd.read_json(filepath, lines=True)
@@ -28,9 +28,9 @@ def process_log_file(cur, filepath):
     # convert timestamp column to datetime
     t = pd.to_datetime(df['ts'], unit='ms')
     df['ts'] = pd.to_datetime(df['ts'], unit='ms')
-
+    
     # insert time data records
-    time_data = [t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday]
+    time_data = (t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday)
     column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
     time_df = pd.DataFrame.from_dict(dict(zip(column_labels, time_data)))
 
@@ -46,18 +46,18 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-
+        
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-
+        
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
--- Data Modeling with Postgres/src/sql_queries.py
+++ Data Modeling with Postgres/src/sql_queries.py
@@ -6,71 +6,69 @@ song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
-
 # CREATE TABLES
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
-        songplay_id SERIAL PRIMARY KEY,
-        start_time TIMESTAMP REFERENCES time (start_time),
-        user_id INT REFERENCES users (user_id),
-        level VARCHAR,
-        song_id VARCHAR REFERENCES songs (song_id),
-        artist_id VARCHAR REFERENCES artists (artist_id),
-        session_id INT,
-        location TEXT,
+        songplay_id SERIAL PRIMARY KEY, 
+        start_time TIMESTAMP REFERENCES time (start_time), 
+        user_id INT REFERENCES users (user_id), 
+        level VARCHAR, 
+        song_id VARCHAR REFERENCES songs (song_id), 
+        artist_id VARCHAR REFERENCES artists (artist_id), 
+        session_id INT, 
+        location TEXT, 
         user_agent TEXT
     )
 """)
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users (
-        user_id INT PRIMARY KEY,
-        first_name VARCHAR,
-        last_name VARCHAR,
-        gender CHAR(1),
+        user_id INT PRIMARY KEY, 
+        first_name VARCHAR, 
+        last_name VARCHAR, 
+        gender CHAR(1), 
         level VARCHAR
     )
 """)
 
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS songs (
-        song_id VARCHAR PRIMARY KEY,
-        title VARCHAR,
-        artist_id VARCHAR REFERENCES artists (artist_id),
-        year INT,
+        song_id VARCHAR PRIMARY KEY, 
+        title VARCHAR, 
+        artist_id VARCHAR REFERENCES artists (artist_id), 
+        year INT, 
         duration FLOAT
     )
 """)
 
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS artists (
-        artist_id VARCHAR PRIMARY KEY,
-        name VARCHAR,
-        location TEXT,
-        latitude FLOAT,
-        longitude FLOAT
+        artist_id VARCHAR PRIMARY KEY, 
+        name VARCHAR, 
+        location TEXT , 
+        latitude FLOAT , 
+        longitude FLOAT 
     )
 """)
 
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time (
-        start_time TIMESTAMP PRIMARY KEY,
-        hour INT,
-        day INT,
-        week INT,
-        month INT,
-        year INT,
+        start_time TIMESTAMP PRIMARY KEY, 
+        hour INT, 
+        day INT, 
+        week INT, 
+        month INT, 
+        year INT, 
         weekday VARCHAR
     )
 """)
 
-
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-    INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
     ON CONFLICT (songplay_id) DO NOTHING
 """)
 
@@ -99,16 +97,15 @@ time_table_insert = ("""
     ON CONFLICT (start_time) DO NOTHING
 """)
 
-
 # FIND SONGS
 
 song_select = ("""
-    SELECT songs.song_id, artists.artist_id
+    SELECT songs.song_id, artists.artist_id 
     FROM songs JOIN artists ON songs.artist_id = artists.artist_id
     WHERE songs.title=%s AND artists.name=%s AND songs.duration=%s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [user_table_create, artist_table_create, time_table_create, song_table_create, songplay_table_create]
+create_table_queries = [artist_table_create, song_table_create, user_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
