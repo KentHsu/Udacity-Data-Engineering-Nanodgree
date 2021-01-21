53628f57e7648487d8358a433e8384771d7a3478
luffy1610 <luffy1610@gmail.com>
2021-01-21 23:56:15 +0800

    fix: dealing with conflicts for insert query
    Modified users_table_insert CONFLICT to update user level when it changes

--- Data Modeling with Postgres/src/sql_queries.py
+++ Data Modeling with Postgres/src/sql_queries.py
@@ -69,13 +69,12 @@ time_table_create = ("""
 songplay_table_insert = ("""
     INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-    ON CONFLICT (songplay_id) DO NOTHING
 """)
 
 user_table_insert = ("""
     INSERT INTO users (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING
+    ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
