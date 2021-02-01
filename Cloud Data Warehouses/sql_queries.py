d32e9ee1e85ef842830202243d95317bf493ace7
luffy1610 <luffy1610@gmail.com>
2021-02-02 01:17:40 +0800

    feat: complete drop/create table queries
    Able to drop/create staging and dimensional tables on Redshift
    See execution result in test.ipynb

--- Cloud Data Warehouses/sql_queries.py
+++ Cloud Data Warehouses/sql_queries.py
@@ -5,37 +5,115 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
+ARN = config.get("IAM_ROLE", "ARN")
+LOG_DATA = config.get("S3", "LOG_DATA")
+LOG_JSONPATH = config.get("S3", "LOG_JSONPATH")
+SONG_DATA = config.get("S3", "SONG_DATA")
+
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+    CREATE TABLE IF NOT EXISTS staging_events (
+        artist VARCHAR,
+        auth VARCHAR,
+        firstName VARCHAR,
+        gender CHAR(1),
+        itemInSession INT,
+        lastName VARCHAR,
+        length FLOAT,
+        level VARCHAR,
+        location TEXT,
+        method VARCHAR,
+        page VARCHAR,
+        registration VARCHAR,
+        sessionId INT,
+        song VARCHAR,
+        status INT,
+        ts BIGINT,
+        userAgent TEXT,
+        userId INT
+    )
 """)
 
 staging_songs_table_create = ("""
+    CREATE TABLE IF NOT EXISTS staging_songs (
+        artist_id VARCHAR,
+        artist_latitude FLOAT,
+        artist_location TEXT,
+        artist_longitude FLOAT,
+        artist_name VARCHAR,
+        duration FLOAT,
+        num_songs INT,
+        song_id VARCHAR,
+        title VARCHAR,
+        year INT
+    )
 """)
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id INT IDENTITY(0,1),
+        start_time TIMESTAMP,
+        user_id INT,
+        level VARCHAR,
+        song_id VARCHAR,
+        artist_id VARCHAR,
+        session_id INT,
+        location TEXT,
+        user_agent TEXT
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INT,
+        first_name VARCHAR,
+        last_name VARCHAR,
+        gender CHAR(1),
+        level VARCHAR
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id VARCHAR,
+        title VARCHAR,
+        artist_id VARCHAR,
+        year INT,
+        duration FLOAT
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id VARCHAR,
+        name VARCHAR,
+        location TEXT ,
+        latitude FLOAT ,
+        longitude FLOAT
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time TIMESTAMP,
+        hour INT,
+        day INT,
+        week INT,
+        month INT,
+        year INT,
+        weekday VARCHAR
+    )
 """)
 
 # STAGING TABLES
