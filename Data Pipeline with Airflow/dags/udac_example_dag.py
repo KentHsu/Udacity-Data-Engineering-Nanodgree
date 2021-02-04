3e29c817dd03fa0217ffea549e1e4a366fafe7e2
luffy1610 <luffy1610@gmail.com>
2021-02-05 02:34:05 +0800

    feat: Configure DAG parameters and task dependencies

--- Data Pipeline with Airflow/dags/udac_example_dag.py
+++ Data Pipeline with Airflow/dags/udac_example_dag.py
@@ -11,12 +11,18 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'start_date': datetime(2018, 11, 1),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False,
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
+          end_date=datetime(2018, 11, 2),          
           schedule_interval='0 * * * *'
         )
 
@@ -63,3 +69,9 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >>\
+load_songplays_table >> [load_user_dimension_table, load_song_dimension_table,\
+                         load_artist_dimension_table, load_time_dimension_table] >>\
+run_quality_checks >> end_operator
